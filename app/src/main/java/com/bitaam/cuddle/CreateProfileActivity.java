86ce19987120e05ee858c88639a54408eafd3072
TechMangalam <201951089@iiitvadodara.ac.in>
2021-08-08 03:13:16 +0530

    creating layouts of fragments

--- app/src/main/java/com/bitaam/cuddle/CreateProfileActivity.java
+++ app/src/main/java/com/bitaam/cuddle/CreateProfileActivity.java
@@ -1,12 +1,15 @@
 package com.bitaam.cuddle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -15,16 +18,26 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bitaam.cuddle.modals.ProfileModal;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
 import java.io.ByteArrayOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -33,14 +46,14 @@ public class CreateProfileActivity extends AppCompatActivity {
 
     AutoCompleteTextView genderExposedDropdown,hobby1ExposedDropdown,hobby2ExposedDropdown,hobby3ExposedDropdown;
     TextInputEditText bioEdt,nameEdt,ageEdt;
-    CircleImageView addImagesCiv;
-    CarouselView carouselView;
+    CircleImageView rotateImgCiv;
+    ImageView imageView;
     Button submitBtn,addLocationBtn;
-    SharedPreferences sharedPreferences;
     ArrayList<String> profileImgUrls;
     ArrayList<Bitmap> selectedImagesBitmaps;
-    Byte[] filesInBytes;
+    byte[] filesInBytes;
     ArrayList<Uri> imgUriList;
+    Bitmap qImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,9 +83,10 @@ public class CreateProfileActivity extends AppCompatActivity {
         nameEdt = findViewById(R.id.name_input);
         ageEdt = findViewById(R.id.age_input);
         bioEdt = findViewById(R.id.bio_input);
-        addImagesCiv = findViewById(R.id.addImage1);
         submitBtn = findViewById(R.id.submitBtn);
         addLocationBtn = findViewById(R.id.addLocationBtn);
+        imageView = findViewById(R.id.profImgView);
+        rotateImgCiv = findViewById(R.id.rotateImgView);
 
         onClickActivities();
 
@@ -94,12 +108,68 @@ public class CreateProfileActivity extends AppCompatActivity {
             }
         });
 
-        addImagesCiv.setOnClickListener(new View.OnClickListener() {
+        TextView i1Tv,i2Tv,i3Tv,i4Tv,i5Tv,i6Tv;
+        i1Tv = findViewById(R.id.addImage1Tv);
+        i2Tv = findViewById(R.id.addImage2Tv);
+        i3Tv = findViewById(R.id.addImage3Tv);
+        i4Tv = findViewById(R.id.addImage4Tv);
+        i5Tv = findViewById(R.id.addImage5Tv);
+        i6Tv = findViewById(R.id.addImage6Tv);
+
+        i1Tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+                i2Tv.setEnabled(true);
             }
         });
+
+        i2Tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+                i3Tv.setEnabled(true);
+            }
+        });
+
+        i3Tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+                i4Tv.setEnabled(true);
+            }
+        });
+
+        i4Tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+                i5Tv.setEnabled(true);
+            }
+        });
+
+        i5Tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+                i6Tv.setEnabled(true);
+            }
+        });
+
+        i6Tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+            }
+        });
+
+
 
     }
 
@@ -161,54 +231,68 @@ public class CreateProfileActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && requestCode == 1000) {
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imgUri = data.getData();
+                Bitmap bmp = null;
+                try{
+                    bmp = MediaStore.Images.Media.getBitmap(getContentResolver(),imgUri);
+                    qImg = bmp;
+                }catch (Exception e){}
+//                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//                bmp.compress(Bitmap.CompressFormat.JPEG,25,baos);
+//                byte[] fileInBytes = baos.toByteArray();
+                filesInBytes = compressImg(qImg);
+                rotateImgCiv.setVisibility(View.VISIBLE);
+                imageView.setImageBitmap(qImg);
 
-
-            assert data != null;
-            if (data.getClipData() != null) {
-
-                int countClipData = data.getClipData().getItemCount();
-                int currentImageSlect = 0;
-
-                while (currentImageSlect < countClipData && currentImageSlect<6) {
-
-                    Uri imgUri = data.getClipData().getItemAt(currentImageSlect).getUri();
-                    imgUriList.add(imgUri);
-                    getImageFromStorage(imgUri);
-                    currentImageSlect = currentImageSlect + 1;
-                }
-
-
-            }else
-                Toast.makeText(this, "Please Select at least 2 Images", Toast.LENGTH_SHORT).show();
+                //uploadImageToFirebase(fileInBytes);
             }
-
         }
-
-
-    private void getImageFromStorage(Uri uri){
-
-        Bitmap bmp = null;
-        try{
-            bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-            selectedImagesBitmaps.add(bmp);
-//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//            bmp.compress(Bitmap.CompressFormat.JPEG,25,baos);
-//            byte[] fileInBytes = baos.toByteArray();
-            //uploadImageToFirebase(fileInBytes);
-        }catch (Exception e){}
-
-
     }
 
+    private Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        filesInBytes = null;
+        filesInBytes = compressImg(rotatedImg);
+        imageView.setImageBitmap(rotatedImg);
+        return rotatedImg;
+    }
 
-    ImageListener imageListener = new ImageListener() {
-        @Override
-        public void setImageForPosition(int position, ImageView imageView) {
-            //Picasso.get().load(profileImgUrls.get(position)).into(imageView);
-            imageView.setImageBitmap(selectedImagesBitmaps.get(position));
+    private byte[] compressImg(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG,25,baos);
+        byte[] fileInBytes = baos.toByteArray();
+        //filesInByte = baos.toByteArray();
+        return fileInBytes;
+    }
 
-        }
-    };
+    private void uploadImageToFirebase(byte[] fileInBytes) {
+
+        //progressBar.setVisibility(View.VISIBLE);
+
+        String imgId = new SimpleDateFormat("MMMM dd, yyyy HH:mm:ss", Locale.getDefault()).format(new Date())+""+ FirebaseAuth.getInstance().getCurrentUser().getUid().toString()+".jpg";
+        final StorageReference fileRef = FirebaseStorage.getInstance().getReference("Images").child(imgId);
+        fileRef.putBytes(fileInBytes).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getApplicationContext(),"Image Uploaded",Toast.LENGTH_SHORT).show();
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        profileImgUrls.add(uri.toString());
+                        //postQuestion();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Some error occurred while uploading image ,try again !",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 }
